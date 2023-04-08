05763c609e4838bf8fa61e8f2657675b948d9dba
Sam Baldwin <sambaldwin92@gmail.com>
2023-04-08 11:48:17 +0100

    Planning second challenge as first was easier than I first thought

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 /* 
 👉 Write your kata here!
+*   
+*   // The first challenge was a little easy for my liking, however I felt it would be better to keep it, rather than delete it.
 *
-*
+*   // Challenge 1 - Easy
+*   
 *   Leap Years!
 *
 *   A leap year occurs every 4 years. Or do they? According to the Gregorian calendar, they occur ALMOST every 4 years.
@@ -16,11 +19,20 @@
 *   Remember to factor in the above criteria 
 *
 *
+*   // Challenge 2 - Medium
+*
+*   Incorrect Product Codes!
+*   
+*   Due to a fault in our database, some of our product codes have been entered incorrectly and are affecting our stock levels.
+*   We need to write a function that will check the product code and return true if it is correct, or false if it is not.
+*
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
 
-export function leapYearChecker(year){
-    //Write your solution here
-    return isLeapYear;
+export function leapYearChecker(year) {
+  //Write your solution here
+  return isLeapYear;
 }
+
+export function productCodeChecker(productCode) {}
