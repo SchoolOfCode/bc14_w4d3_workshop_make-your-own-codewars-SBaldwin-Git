f76dcf42096ce8273e0ab89b0718094983f7aaae
Sam Baldwin <sambaldwin92@gmail.com>
2023-04-08 14:01:13 +0100

    Further cleaned files

--- main.js
+++ main.js
@@ -1,25 +1,5 @@
 /* 
 👉 Write your kata here!
-*   
-*   // The first challenge was a little easy for my liking, however I felt it would be better to keep it, rather than delete it.
-*
-*   // Challenge 1 - Easy
-*   
-*   Leap Years!
-*
-*   A leap year occurs every 4 years. Or do they? According to the Gregorian calendar, they occur ALMOST every 4 years.
-*
-*   // The conditions include //
-*
-*   - That the year MUST be a multiple of 4
-*   - It is NOT a multiple of 100
-*   - UNLESS it is a multiple of 400
-*
-*   Given a number of days as an input, please return true if the year provided is a leap year, or false if it is not.
-*   Remember to factor in the above criteria 
-*
-*
-*   // Challenge 2 - Medium
 *
 *   Incorrect Product Codes!
 *   
@@ -37,16 +17,9 @@
 *   e.g 8ehsisn832794792826363hshs should return "Invalid Product Code"
 *
 *   The initial code provided will be an alphanumeric string, and will not include special characters.
-*
-*
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
-
-export function leapYearChecker(year) {
-  //Write your solution here
-  return isLeapYear;
-}
 
 export function productCodeChecker(productCode){
     
