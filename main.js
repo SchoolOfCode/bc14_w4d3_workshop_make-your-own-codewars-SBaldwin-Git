6e0f4d8dfe3fa379b4bc4e7e2c51b6828353de80
Sam Baldwin <sambaldwin92@gmail.com>
2023-04-08 10:14:15 +0100

    Added leap year test description and emprty function in main

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 /* 
 👉 Write your kata here!
+*
+*
+*   Leap Years!
+*
+*   A leap year occurs ALMOST every 4 years according to the Gregorian calendar.
+*
+*   // The conditions include //
+*
+*   - That the year MUST be a multiple of 4
+*   - It is NOT a multiple of 100
+*   - UNLESS it is a multiple of 400
+*
+*   Given a number of years as an input, please return true if the year provided is a leap year, or false if it is not.
+*
+*
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+export function leapYearChecker(year){
+    //Write your solution here
+    return isLeapYear;
+}
